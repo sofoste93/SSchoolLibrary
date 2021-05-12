3bc78410243e9644297690727fe827925ed5d355
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2021-05-12 22:07:51 +0200

    Create EnglishPaper.cs
    Co-Authored-By: Stephane Sob Fouodji <stefan-kruger7@hotmail.com>

---
+++ SchoolApp/SchoolLibrary/EnglishPaper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolLibrary
+{
+	class EnglishPaper
+	{
+		public string Title { get; set; }
+		public int MinimumWordCount { get; set; }
+		public string PaperText { get; set; }
+	}
+}
