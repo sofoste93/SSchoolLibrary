896419f622e9f0484da30b376679e1905b067c12
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2021-05-12 21:51:01 +0200

    Update Person.cs
    Implement SendMessage()
    
    Co-Authored-By: Stephane Sob Fouodji <stefan-kruger7@hotmail.com>

--- SchoolApp/SchoolLibrary/Person.cs
+++ SchoolApp/SchoolLibrary/Person.cs
@@ -13,8 +13,21 @@ namespace SchoolLibrary
 		public string LastName { get; set; }
 		public string Email { get; set; }
 
-
+		// assign grade
 		public abstract float ComputeGradeAVG();
+
+		// send Message - Virtual Method
+		public virtual string SendMessage(string message)
+		{
+			var sBuilder = new StringBuilder();
+			var timeStamp = string.Format("Sent on {0:D} at {0:t}", DateTime.Now);
+			sBuilder.AppendLine(timeStamp);
+			sBuilder.AppendLine("");
+			sBuilder.AppendLine("Dear " + FirstName + ",");
+			sBuilder.AppendLine(message);
+
+			return sBuilder.ToString();
+		}
 
 	}
 }
