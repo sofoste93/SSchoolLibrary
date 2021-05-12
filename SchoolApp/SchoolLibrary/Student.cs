4ecb7d139228dc6cd70cc5b9cbe4b730632dbf69
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2021-05-12 21:57:05 +0200

    Update Student.cs
    Overriding virtual Method in subclasses
    
    Co-Authored-By: Stephane Sob Fouodji <stefan-kruger7@hotmail.com>

--- SchoolApp/SchoolLibrary/Student.cs
+++ SchoolApp/SchoolLibrary/Student.cs
@@ -17,5 +17,16 @@ namespace SchoolLibrary
 			return gAvg;
 			// throw new NotImplementedException();
 		}
+
+		// send Msg
+		public override string SendMessage(string message)
+		{
+			// return base.SendMessage(message);
+			var originalMsg = base.SendMessage(message);
+			var sBuilder = new StringBuilder(originalMsg);
+			sBuilder.AppendLine("This message is private and confidential.");
+
+			return sBuilder.ToString();
+		}
 	}
 }
