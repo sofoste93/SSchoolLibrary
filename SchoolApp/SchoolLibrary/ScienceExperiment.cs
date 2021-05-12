1f2cef406978c6d28da7bda5d8f8ebe529789d45
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2021-05-12 22:11:19 +0200

    Create ScienceExperiment.cs
    Co-Authored-By: Stephane Sob Fouodji <stefan-kruger7@hotmail.com>

---
+++ SchoolApp/SchoolLibrary/ScienceExperiment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolLibrary
+{
+	class ScienceExperiment
+	{
+		public string Hypothesis { get; set; }
+		public string Materials { get; set; }
+		public string Method { get; set; }
+		public string Conclusion { get; set; }
+	}
+}
