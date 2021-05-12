58ee26d455b7e3957704f86a9bc4a5fd06af1c1b
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2021-05-12 22:42:47 +0200

    Create ExtensionMethods.cs
    Co-Authored-By: Stephane Sob Fouodji <stefan-kruger7@hotmail.com>

---
+++ SchoolApp/SchoolLibrary/ExtensionMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolLibrary
+{
+	// extension method must be defined in a non-generic static class
+	public static class ExtensionMethods // update adding keyword 'static
+	{
+		// static method
+		public static int WordCount(this string myString)
+		{
+			var wordCount = myString.Split(new char[] { ' ', '.', '?' },
+				StringSplitOptions.RemoveEmptyEntries).Length;
+
+			return wordCount;
+		}
+	}
+}
