4a8c5d322f4b1c8cdcd50e1ada116978e1999e0f
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2021-05-12 22:44:10 +0200

    Update EnglishPaper.cs
    Extension methods
    
    Co-Authored-By: Stephane Sob Fouodji <stefan-kruger7@hotmail.com>

--- SchoolApp/SchoolLibrary/EnglishPaper.cs
+++ SchoolApp/SchoolLibrary/EnglishPaper.cs
@@ -15,5 +15,10 @@ namespace SchoolLibrary
 		public string Title { get; set; }
 		public int MinimumWordCount { get; set; }
 		public string PaperText { get; set; }
+
+		public int WordCount
+		{
+			get { return PaperText.WordCount(); }
+		}
 	}
 }
